52abae76d28092ee71cada720dfae1a34347507b
Orjihenry <orjihen@yahoo.com>
2024-01-20 23:36:58 -0330

    Star wars characters 👍 👍 😎

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,46 +1,25 @@
 #!/usr/bin/node
-// Print starwars characters
-
 const request = require('request');
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
-// Check if Movie ID is provided as a command-line argument
-const movieId = process.argv[2];
-if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <movie_id>');
-  process.exit(1);
-}
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
 
-const Url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-request(Url, (error, response, body) => {
-  if (error) {
-    console.error(`Error fetching movie data: ${error.message}`);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error(`Error fetching movie data. Status Code: ${response.statusCode}`);
-    return;
-  }
-
-  const charactersUrls = JSON.parse(body).characters;
-
-  // Fetch character names for each URL
-  charactersUrls.forEach(characterUrl => {
-    request(characterUrl, (characterError, characterResponse, characterBody) => {
-      if (characterError) {
-        console.error(`Error fetching character data: ${characterError.message}`);
-        return;
-      }
-
-      if (characterResponse.statusCode === 200) {
-        const characterName = JSON.parse(characterBody).name;
-        console.log(characterName);
-      } else {
-        console.error(`Error fetching character data. Status Code: ${characterResponse.statusCode}`);
-      }
-    });
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
   });
-});
-
-// getCast(movieId);
+}
